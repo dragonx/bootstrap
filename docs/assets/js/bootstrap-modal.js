037fc8bffdf06237b018176daa4cce4a87fa667b
projecteat <projecteatapp@gmail.com>
2013-07-05 17:17:48 -0400

    Disable enforceFocus(), which was causing an infinite loop when two modals were both onscreen (edit menu page).

--- docs/assets/js/bootstrap-modal.js
+++ docs/assets/js/bootstrap-modal.js
@@ -70,7 +70,7 @@
             .addClass('in')
             .attr('aria-hidden', false)
 
-          that.enforceFocus()
+          //that.enforceFocus()
 
           transition ?
             that.$element.one($.support.transition.end, function () { that.$element.focus().trigger('shown') }) :
